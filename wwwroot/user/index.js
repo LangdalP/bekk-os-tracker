dc0065f099e45fd033c10830a4e5277ee3694166
Peder Voldnes Langdal <peder.langdal@gmail.com>
2018-02-26 21:23:04 +0100

    Tester GitHub-integrasjon

--- wwwroot/user/index.js
+++ wwwroot/user/index.js
@@ -4,27 +4,28 @@ const client_id = process.env.GITHUB_CLIENT_ID;
 const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
 const request_settings = {
-    json: true,
-    headers: {
-        'User-Agent': 'request'
-    }
+  json: true,
+  headers: {
+    'User-Agent': 'request'
+  }
 };
 
 module.exports = function (context, req) {
-    if (req.query.name || (req.body && req.body.name)) {
-        request(`https://api.github.com/users/defunkt?client_id=${client_id}&client_secret=${client_secret}` , request_settings, (err, res, body) => {
-            if (err) { return console.log(err); }
-            context.log(JSON.stringify(body));
-            context.done(err, {
-                // status: 200, /* Defaults to 200 */
-                body: "Hello " + (req.query.name || req.body.name)
-            });
-        });
-    }
-    else {
-        context.done(null, {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        });
-    }
+  if (req.query.name || (req.body && req.body.name)) {
+    request(`https://api.github.com/users/${req.body.name}?client_id=${client_id}&client_secret=${client_secret}` , request_settings, (err, res, body) => {
+      if (err) {
+        context.log(err);
+        context.done(null, { status: 500 });
+      }
+      context.done(err, {
+        body: body
+      });
+    });
+  }
+  else {
+    context.done(null, {
+      status: 400,
+      body: "Please pass a name on the query string or in the request body"
+    });
+  }
 };
