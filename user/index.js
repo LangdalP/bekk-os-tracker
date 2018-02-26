283685bbcbd0230a9dee9268d36c558e5e9dc60b
Peder Voldnes Langdal <peder.langdal@gmail.com>
2018-02-26 22:14:11 +0100

    Skal aldri lese name frå body

--- user/index.js
+++ user/index.js
@@ -11,8 +11,8 @@ const request_settings = {
 };
 
 module.exports = function (context, req) {
-  if (req.query.name || (req.body && req.body.name)) {
-    request(`https://api.github.com/users/${req.body.name}?client_id=${client_id}&client_secret=${client_secret}` , request_settings, (err, res, body) => {
+  if (req.query.name) {
+    request(`https://api.github.com/users/${req.query.name}?client_id=${client_id}&client_secret=${client_secret}` , request_settings, (err, res, body) => {
       if (err) {
         context.log(err);
         context.done(null, { status: 500 });
