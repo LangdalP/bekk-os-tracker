b62030c72d129527302c237ff324925171f2f87a
Peder Voldnes Langdal <peder.langdal@gmail.com>
2018-03-15 23:27:07 +0100

    Endra query-parameter for GH-brukarnamn

--- prs/index.js
+++ prs/index.js
@@ -3,9 +3,9 @@ var utils = require('../utils.js')
 module.exports = function (context, req) {
   let pullReqs = context.bindings.tableBinding
     .map(utils.prWithParsedDate);
-  if (req.query.name) {
+  if (req.query.user) {
     pullReqs = pullReqs.filter(
-      pr => pr.PartitionKey.toLowerCase() === req.query.name.toLowerCase())
+      pr => pr.PartitionKey.toLowerCase() === req.query.user.toLowerCase())
   }
   pullReqs.sort(utils.sortPrsMostRecentFirst);
   context.done(null, {
